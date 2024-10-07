eb37222ddd44963d5b700d76ae3ee0bd30feda47
truongleminhnghia <adronghia@gmail.com>
2024-10-07 09:01:12 +0700

    add client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/')
+            .then(response => {
+                setMessage(response.data);
+            })
+            .catch(error => {
+                console.error('There was an error fetching the data!', error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    );
+}
+
+export default App;
